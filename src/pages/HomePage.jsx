4851b68eec86962a3f1ec1cd13cc8a83af002e65
SHAHUL-AHMED-77 <shahul77235@gmail.com>
2026-03-01 22:54:51 +0530

    Adjusted padding

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,7 +6,7 @@ import Footer from "../components/home/Footer";
 const HomePage = () => {
   return (
     <HomeLayout>
-      <div className="relative z-10 flex flex-col items-center px-5 pt-24 pb-20">
+      <div className="relative z-10 flex flex-col items-center px-5 pt-24 pb-10">
         <div className="w-full max-w-6xl grid grid-cols-1 lg:grid-cols-2 items-stretch">
           <CharacterCard />
           <RightContent />
