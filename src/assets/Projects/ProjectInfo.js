264cbf4a182fa73c524be36097c4882812d2dee2
SHAHUL-AHMED-77 <shahul77235@gmail.com>
2026-04-10 15:48:29 +0530

    add : added project page image

---
+++ src/assets/Projects/ProjectInfo.js
@@ -0,0 +1,169 @@
+import img1 from "../Projects/img1.png";
+import img2 from "../Projects/img2.png";
+import img3 from "../Projects/img3.png";
+import img4 from "../Projects/img4.png";
+import img5 from "../Projects/img5.png";
+import img6 from "../Projects/img6.png";
+import img7 from "../Projects/img7.png";
+import img8 from "../Projects/img8.png";
+import img9 from "../Projects/img9.png";
+import img10 from "../Projects/img10.png";
+import img11 from "../Projects/img11.png";
+import img12 from "../Projects/img12.png";
+import img13 from "../Projects/img13.png";
+import img14 from "../Projects/img14.png";
+import img15 from "../Projects/img15.png";
+
+export const project = [
+  {
+    id: 15,
+    name: "MediaVerse",
+    desc: "A full-stack MERN media tracker that allows users to track, analyze, and share their favorite movies, series, anime, books, and games. It includes analytics, badge system, customizable profiles, and community features for following other users.",
+    image: img15,
+    github: "https://github.com/SyedShahulAhmed/MediaVerse.git",
+    preview: "https://mediaverse-seven.vercel.app/",
+    categories: [
+      "React Js",
+      "Node Js",
+      "Express Js",
+      "MongoDB",
+      "Tailwind Css",
+    ],
+    button: true,
+  },
+
+  {
+    id: 14,
+    name: "Postify",
+    desc: "A full-featured post management app built with React.js and Tailwind CSS, using Appwrite for backend services. It allows users to create, update, and delete posts with seamless authentication and data storage.",
+    image: img14,
+    github: "https://github.com/SyedShahulAhmed/Postify.git",
+    preview: "https://postify-beta-ashen.vercel.app/",
+    categories: ["React Js", "Tailwind Css", "Appwrite"],
+    button: true,
+  },
+  {
+    id: 13,
+    name: "PlayTube",
+    desc: "A YouTube-like clone built with React.js and Tailwind CSS, utilizing the official YouTube API. It features video viewing and category-based filtering for a seamless user experience.",
+    image: img13,
+    github: "https://github.com/SyedShahulAhmed/PlayTube.git",
+    preview: "https://playtube-eta.vercel.app/",
+    categories: ["React Js", "Tailwind Css", "YouTube API"],
+    button: true,
+  },
+  {
+    id: 12,
+    name: "Netflix Clone",
+    desc: "A Netflix UI clone built with React.js, Tailwind CSS, and Firebase, featuring login and signup functionality. It replicates Netflix's interface with static content and a modern, sleek design",
+    image: img12,
+    github: "https://github.com/SyedShahulAhmed/Netflix.git",
+    preview: "https://netflix-gamma-peach-44.vercel.app/",
+    categories: ["React Js", "Tailwind Css", "Firebase"],
+    button: true,
+  },
+  {
+    id: 1,
+    name: "ReactXhibit",
+    desc: "ReactXhibit is a curated collection of engaging mini React projects, each designed to demonstrate core React concepts through hands-on examples. With interactive features, smooth animations, and practical implementations",
+    image: img5,
+    github: "https://github.com/SyedShahulAhmed/ReactXhibit.git",
+    preview: "https://reactxhibit.vercel.app/",
+    categories: ["React Js", "Tailwind Css"],
+    button: true,
+  },
+  {
+    id: 2,
+    name: "Spotify Clone",
+    desc: "A Spotify clone featuring albums, songs, and popular artists, with seamless music playback functionality. Built with React and Tailwind CSS for a sleek and responsive music streaming experience.",
+    image: img2,
+    github: "https://github.com/SyedShahulAhmed/Spotify-Clone.git",
+    preview: "https://spotifyclone-topaz.vercel.app/",
+    categories: ["React Js", "Tailwind Css"],
+    button: true,
+  },
+  {
+    id: 3,
+    name: "AstroHub",
+    desc: "AstroHub is a blog website that features blogs, a contact page, login functionality, and services pages, all built with React and styled using Tailwind CSS for a modern and responsive design",
+    image: img3,
+    github: "https://github.com/SyedShahulAhmed/AstroHub.git",
+    preview: "https://astrohub-omega.vercel.app/",
+    categories: ["React Js", "Tailwind Css"],
+    button: true,
+  },
+  {
+    id: 4,
+    name: "Cartly",
+    desc: "Cartly is a website where users can add items to the cart, remove them, and preview the contents of their cart and bag, all built with React and styled using Tailwind CSS for a smooth user experience",
+    image: img4,
+    github: "https://github.com/SyedShahulAhmed/Cartly.git",
+    preview: "https://cartly-six.vercel.app/",
+    categories: ["React Js", "Tailwind Css"],
+    button: true,
+  },
+  {
+    id: 5,
+    name: "CodeXhibit",
+    desc: "Showcasing 35 mini JavaScript projects, including interactive features, animations, and practical examples, ideal for learning and mastering essential JavaScript concepts with hands-on experience",
+    image: img1,
+    github: "https://github.com/SyedShahulAhmed/JavacriptProjects.git",
+    preview: "https://js-project-show-case.vercel.app/",
+    categories: ["React Js", "Tailwind Css"],
+    button: true,
+  },
+  {
+    id: 6,
+    name: "FlavourSphere",
+    desc: "FlavourSphere is a website where users can search for recipes and view detailed recipe instructions, all powered by API integration for a dynamic and responsive cooking experience",
+    image: img6,
+    github: "https://github.com/SyedShahulAhmed/FlavourSphere.git",
+    preview: "https://flavoursphere-omega.vercel.app/",
+    categories: ["React Js", "Tailwind Css"],
+    button: true,
+  },
+  {
+    id: 7,
+    name: "Lucy-Virtual-Assistant",
+    desc: "Lucy is a voice assistant created using Python, leveraging libraries like SpeechRecognition, Pyttsx3, and Requests for speech-to-text, text-to-speech, and web requests, offering a hands-free interactive experience.",
+    image: img7,
+    github: "https://github.com/SyedShahulAhmed/Lucy-Virtual-Assistant.git",
+    preview: "https://github.com/SyedShahulAhmed/Lucy-Virtual-Assistant.git",
+    categories: ["Python", "SpeechRecognition", "Pyttsx3"],
+  },
+  {
+    id: 8,
+    name: "PS5 Website",
+    desc: "PS5 Website is a responsive site built with HTML and CSS, showcasing the PlayStation 5 with detailed information, images, and specifications for an engaging user experience.",
+    image: img8,
+    github: "https://github.com/SyedShahulAhmed/Ps5-Website.git",
+    preview: "https://ps5-website.vercel.app/",
+    categories: ["HTML", "CSS", "Responsive"],
+    button: true,
+  },
+  {
+    id: 9,
+    name: "Royal Insights",
+    desc: "A dynamic newspaper website built with HTML, CSS, JavaScript, and PHP, featuring news articles, categories, and real-time content updates for an engaging user experience.",
+    image: img9,
+    github: "https://github.com/SyedShahulAhmed/NewsPaper.git",
+    preview: "https://github.com/SyedShahulAhmed/NewsPaper.git",
+    categories: ["HTML", "CSS", "JavaScript", "PHP"],
+  },
+  {
+    id: 10,
+    name: "Diwali Sales Analysis",
+    desc: "An in-depth analysis of Diwali sales data using Python, uncovering insights into buyer demographics, purchasing patterns, and top-selling product categories.",
+    image: img10,
+    github: "https://github.com/SyedShahulAhmed/Diwali-Sales-Analysis.git",
+    categories: ["Python", "Pandas", "Seaborn"],
+  },
+  {
+    id: 11,
+    name: "Customer Churn Analysis",
+    desc: "A comprehensive analysis of customer churn using Python, identifying key factors driving churn and predicting churn probabilities.",
+    image: img11,
+    github: "https://github.com/SyedShahulAhmed/Customer-Churn-Analysis.git",
+    categories: ["Python", "Numpy", "Pandas"],
+  },
+];
